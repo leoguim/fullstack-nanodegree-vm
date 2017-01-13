b984f1b35208f46850b00305769586f48764c65f
leoguim <leo_guim@hotmail.com>
2017-01-13 15:19:07 -0500

    Changes to Tournament Project

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -7,71 +7,70 @@ import psycopg2
 
 
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
-    """Remove all the match records from the database."""
+    DB = connect()
+    cur = DB.cursor()
+    cur.execute ("DELETE FROM matches")
+    DB.commit()
+    DB.close()
 
 
 def deletePlayers():
-    """Remove all the player records from the database."""
-
+    DB = connect()
+    cur = DB.cursor()
+    cur.execute ("DELETE FROM players")
+    DB.commit()
+    DB.close()
 
 def countPlayers():
-    """Returns the number of players currently registered."""
+    DB = connect()
+    cur = DB.cursor()
+    cur.execute ("SELECT COUNT(player_id) from players")
+    counts = cur.fetchone()
+    return counts[0]
+    DB.close()
 
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
-  
-    Args:
-      name: the player's full name (need not be unique).
-    """
+    DB = connect()
+    cur = DB.cursor()
+    cur.execute ("INSERT INTO players (name) VALUES (%s)", (name,))
+    DB.commit()
+    DB.close()
 
 
 def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
-
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
-
-    Returns:
-      A list of tuples, each of which contains (id, name, wins, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        wins: the number of matches the player has won
-        matches: the number of matches the player has played
-    """
+    DB = connect()
+    cur = DB.cursor()
+    cur.execute("SELECT * FROM v_summary")
+    record = cur.fetchall()
+    DB.close()
+    return record
 
 
 def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
+    DB = connect()
+    cur = DB.cursor()
+    cur.execute ("INSERT INTO matches (player_win,player_lose) VALUES (%s, %s)", [winner,loser])
+    DB.commit()
+    DB.close()
 
-    Args:
-      winner:  the id number of the player who won
-      loser:  the id number of the player who lost
-    """
- 
  
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
-  
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
+    DB = connect()
+    cur = DB.cursor()
+    cur.execute ("SELECT * FROM v_summary")
+    records = cur.fetchall()
+    pairings = []
+    num_players = len(records)
+    for x in range (0, num_players-1, 2):
+        pair = (records[x][0],records[x][1],records[x+1][0],records[x+1][1])
+        pairings.append(pair)
+    DB.close()
+    return pairings
+    
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP table players;
+DROP table matches;
+DROP table v_summary;
 
+CREATE table players (player_id serial primary key, 
+	name text);
+
+CREATE table matches (matches_id serial primary key,
+	player_win integer references players (player_id), 
+	player_lose integer references players (player_id));
+
+CREATE view v_summary as
+select players.player_id, players.name,
+(select count(*) from matches where matches.player_win = players.player_id ) as wins,
+(select count(*) from matches where players.player_id in (matches.player_win, matches.player_lose) ) as played
+from players
+order by wins desc, played asc;
